3f1909ed507060ce82d242035311e3394ac3777f
Rose Kramer <kramr970@newschool.edu>
2017-05-11 09:41:39 -0400

    new

--- 70sblog/assets/js/main.js
+++ 70sblog/assets/js/main.js
@@ -1,3 +1,84 @@
+$(".text").on({
+    mouseover: function() {
+        $(".coshover").stop().show();
+        console.log("hello");
+    },
+    
+    mouseout: function() {
+        $(".coshover").stop().hide();
+    }
+})
+
+$(".text1").on({
+    mouseover: function() {
+        $(".maghover").stop().show();
+    },
+    
+    mouseout: function() {
+        $(".maghover").stop().hide();
+    }
+})
+
+$(".text").on({
+    mouseover: function() {
+        $(".prohover").stop().show();
+    },
+    
+    mouseout: function() {
+        $(".prohover").stop().hide();
+    }
+})
+
+$(".text").on({
+    mouseover: function() {
+        $(".movhover").stop().show();
+    },
+    
+    mouseout: function() {
+        $(".movhover").stop().hide();
+    }
+})
+
+
+$(".text").on({
+    mouseover: function() {
+        $(".stylehover").stop().show();
+    },
+    
+    mouseout: function() {
+        $(".stylehover").stop().hide();
+    }
+})
+
+$(".text").on({
+    mouseover: function() {
+        $(".racehover").stop().show();
+    },
+    
+    mouseout: function() {
+        $(".racehover").stop().hide();
+    }
+})
+
+$(".text").on({
+    mouseover: function() {
+        $(".rechover").stop().show();
+    },
+    
+    mouseout: function() {
+        $(".rechover").stop().hide();
+    }
+})
+$(".text").on({
+    mouseover: function() {
+        $(".carhover").stop().show();
+    },
+    
+    mouseout: function() {
+        $(".carhover").stop().hide();
+    }
+})
+
 var slideIndex = 1;
 showDivs(slideIndex);
 
@@ -14,4 +95,7 @@ function showDivs(n) {
      x[i].style.display = "none";  
   }
   x[slideIndex-1].style.display = "block";  
+
 }
+
+
